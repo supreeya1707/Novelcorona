7aafa10da742ae06eb63e7b2378b3d29570e172f
supreeya <supreeya1707@gmail.com>
2021-04-22 16:34:48 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsComponent } from './forms/forms.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     FormsComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-forms',
@@ -7,11 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
   radioGender: any = 0;
-  radioPreg:any=0;
-  radioCheck :any=0;
-  radioAddress:any=0;
-  radioSmoke : any=0;
-  constructor() { }
+  radioPreg: any = 0;
+  radioCheck: any = 0;
+  radioAddress: any = 0;
+  radioSmoke: any = 0;
+
+
+  currentDate = new Date();
+  datadate: any;
+
+  constructor() {
+  }
+
+  form = new FormGroup({
+    dateYMD: new FormControl(new Date()),
+    dateFull: new FormControl(new Date()),
+    dateMDY: new FormControl(new Date()),
+    dateRange: new FormControl([
+      new Date(),
+      new Date(this.currentDate.setDate(this.currentDate.getDate() + 7))
+
+    ])
+  });
 
   ngOnInit(): void {
   }
