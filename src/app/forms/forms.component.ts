9ccca205caadb3a2430d41f08d3c85d320e31fe7
supreeya <supreeya1707@gmail.com>
2021-04-29 16:40:54 +0700

    update29/04/2564

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import * as moment from 'moment';
-import { BsLocaleService } from 'ngx-bootstrap/datepicker';
-import { listLocales } from 'ngx-bootstrap/chronos';
+import {BsLocaleService} from 'ngx-bootstrap/datepicker';
+import {listLocales} from 'ngx-bootstrap/chronos';
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
@@ -26,34 +27,40 @@ export class FormsComponent implements OnInit {
   radioinject: any = 0;
   radiolabtest: any = 0;
   radiotest: any = 0;
-  dateStart = moment().format('yyyy-MM-DD');
+  dateTimeLine: any = [];
+  dateTimeLineShort: any = [];
+
+  sDate: any;
+  // dateStart = moment().format('yyyy-MM-DD');
+  dateStart = moment().locale('th').add(543, 'year').format('DD/MM/yyyy');
+
   // dateFirst = moment().format('yyyy-MM-DD');
 
-  name:string='';
-  cid:string='';
-  national:string='';
-  numPreg:string='';
-  pregAge:string='';
+  name: string = '';
+  cid: string = '';
+  national: string = '';
+  numPreg: string = '';
+  pregAge: string = '';
 
-  station:string='';
-  job:string='';
-  Telephone:string='';
-  Telephonedoc:string='';
-  treat:string='';
-  No:string='';
-  moo:string='';
-  mooban:string='';
-  soi:string='';
-  road:string='';
-  tumbon:string='';
-  amphur:string='';
-  symtomany:string='';
-  province:string='';
-  hos_first:string='';
-  province_first:string='';
-  hos_now:string='';
-  province_now:string='';
-  find_symtom:string='';
+  station: string = '';
+  job: string = '';
+  Telephone: string = '';
+  Telephonedoc: string = '';
+  treat: string = '';
+  No: string = '';
+  moo: string = '';
+  mooban: string = '';
+  soi: string = '';
+  road: string = '';
+  tumbon: string = '';
+  amphur: string = '';
+  symtomany: string = '';
+  province: string = '';
+  hos_first: string = '';
+  province_first: string = '';
+  hos_now: string = '';
+  province_now: string = '';
+  find_symtom: string = '';
   locale = 'th-be';
   locales = listLocales();
   currentDate = new Date();
@@ -76,6 +83,8 @@ export class FormsComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.dateStart);
     this.localeService.use(this.locale);
+    this.genDateTimeLine(this.dateStart);
+    console.log(this.dateTimeLine);
   }
 
   onCheckboxChange(e) {
@@ -83,9 +92,37 @@ export class FormsComponent implements OnInit {
       this.radiosymptom.push(e.target.value);
     } else {
       const index = this.radiosymptom.indexOf(e.target.value);
-      this.radiosymptom.splice(index,1);
+      this.radiosymptom.splice(index, 1);
     }
     console.log(this.radiosymptom);
+  }
+
+  convertDate(d: any, i: any): any {
+    const ss: any = d.toString().split('/');
+    const dataDate: any = (ss[2] - 543) + '-' + ss[1] + '-' + ss[0];
+    const datai: any = -i;
+    return moment(dataDate).locale('th').add(datai, 'day').format('DD MMMM YYYY');
+  }
+
+  genDateTimeLine(e: any): any {
+
+    for (let i = 1; i <= 14; i++) {
+      this.dateTimeLine.push(this.convertDate(e, i));
+      this.dateTimeLineShort.push(moment(this.sDate).add(-i, 'day').format('YYYY-MM-DD'));
+    }
+    console.log(this.dateStart)
+  }
+
+  selectDateStart(e: any): any {
+    console.log(e);
+    this.dateStart = moment(e).add(543, 'year').format('DD/MM/YYYY');
+    this.sDate = moment(e).format('YYYY-MM-DD');
+    console.info(this.dateStart);
+    this.dateTimeLine = [];
+    this.dateTimeLineShort = [];
+    this.genDateTimeLine(this.dateStart);
+    console.warn(this.dateTimeLineShort);
+
   }
 
 }
