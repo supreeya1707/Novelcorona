aceda7b8ee6037f3b03b8797e39dff5159dd4c76
supreeya <supreeya1707@gmail.com>
2021-07-14 17:57:45 +0700

    update14/07/2564

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {FormsComponent} from "./forms/forms.component";
 import {ReportComponent} from "./report/report.component";
-import {RegisterComponent} from "./register/register.component";
+// import {RegisterComponent} from "./register/register.component";
 
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'form'},
   {path: 'form', component: FormsComponent},
   {path: 'report', component: ReportComponent},
-  {path: 'register', component: RegisterComponent},
+  // {path: 'register', component: RegisterComponent},
 
 
   // {path:'**',redirectTo:'date'}
